b6569559ff48c71239568ac4b5d60d8a09b40450
AndreyPanovA <a.andrey.panov@gmail.com>
2021-05-03 14:46:56 +0300

    T

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -5,6 +5,7 @@ import cn from "classnames"
 import cls from "./style.module.scss"
 // interfaces
 import {InputTarget} from "../../interfaces"
+
 // icons
 import { FaRegTimesCircle,FaCheck, FaChevronRight } from 'react-icons/fa';
 import { GREEN, RED } from "../../constants";
