f47713c2299f9f7eef053e8a3f243fdcc55a0d4d
AndreyPanovA <a.andrey.panov@gmail.com>
2021-06-05 20:16:48 +0300

    links

--- src/components/time-block-info/index.tsx
+++ src/components/time-block-info/index.tsx
@@ -16,9 +16,10 @@ interface Props {
     title?:{
         top?:string,
         bottom?:string
-    }
+    },
+    href?:string
 }
-const TimeText:FC<Props> = ({hours="17", minutes="00", mounth="07",year="2021", type="time", title})=> {
+const TimeText:FC<Props> = ({hours="17", minutes="00", mounth="07",year="2021", type="time", title, href="https://yandex.ru/maps/-/CCUaNRQ~GC"})=> {
     const Icon =type=="date" ? IoCalendarOutline :type=="time" ? WiTime4:WiTime4
     return (
         <div className={cn(cls.timeContainer)} >
@@ -28,7 +29,7 @@ const TimeText:FC<Props> = ({hours="17", minutes="00", mounth="07",year="2021", 
                 {type=="text" ? 
                 <div className={cn(cls.small)}>
                     <h3>{title?.top}</h3>
-                    <h3>{title?.bottom}</h3>
+                    <a href={href} className={cls.a}>{title?.bottom}</a>
                 </div>
                 :
                 <div className={cn(cls.timeContainer)}>
@@ -52,12 +53,16 @@ const TimeBlockInfo =()=> {
             <div className={cn(cls.container)}>
                 <TimeText type="date"/>
                 <TimeText hours={11} />
-                <TimeText type="text" title={{top:"Башня “Око”, Ресторан “Birds”", bottom:"Москва, Первый Красногвардейский проезд, дом 21 стр. 2"}} />
+                <TimeText type="text" title={{top:"Башня “Око”, Ресторан “Birds”", bottom:"Москва, Первый Красногвардейский проезд, дом 21 стр. 2"}} 
+                    href="https://yandex.ru/maps/-/CCUaNRQ~GC"
+                />
             </div>
             <div className={cn(cls.container)}>
                 <TimeText type="date" hours={18} />
                 <TimeText hours={15}  />
-                <TimeText type="text" title={{top:"Загородный дом “Fisherix”", bottom:"Москва, Посёлок “Щаповское”, улица Евгения Родионова, вл. 7, ГК “Fisherix”"}} />
+                <TimeText type="text" title={{top:"Загородный дом “Fisherix”", bottom:"Москва, Посёлок “Щаповское”, улица Евгения Родионова, вл. 7, ГК “Fisherix”"}} 
+                href="https://yandex.ru/maps/-/CCUaNRWR8D"
+                />
             </div>
         </div>
     )
